1f4cd36f71307f5069c65094e82f8d4a9a7e5bc4
leedex <nickyhex3@gmail.com>
2026-04-24 16:43:57 +0200

    mobile upgrade

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SLIDER
 let slides = document.querySelectorAll(".slide");
 let i = 0;
 
@@ -6,9 +7,17 @@ function changeSlide(){
     slides[i].classList.add("active");
 
     i++;
-    if(i >= slides.length){
-        i = 0;
-    }
+    if(i >= slides.length) i = 0;
 }
 
 setInterval(changeSlide, 4000);
+
+// MOBILE MENU
+function toggleMenu(){
+    let menu = document.getElementById("navLinks");
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
