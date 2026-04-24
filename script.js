8c9bcbfb0255fda31889919ede42c018e0aa9261
leedex <nickyhex3@gmail.com>
2026-04-24 15:06:58 +0200

    first upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll(".slide");
+let i = 0;
+
+function changeSlide(){
+    slides.forEach(s => s.classList.remove("active"));
+    slides[i].classList.add("active");
+
+    i++;
+    if(i >= slides.length){
+        i = 0;
+    }
+}
+
+setInterval(changeSlide, 4000);
